d0131629a51d0df5fee4782841282600d38c9086
spudymun <spudymun@gmail.com>
2021-07-02 17:43:44 +0300

    second commit

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000"); err != nil {
 		log.Fatalf("error occured while runnng httserver: %s", err.Error())
